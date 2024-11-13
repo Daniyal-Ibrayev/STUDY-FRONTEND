1c12a261a4350ff9036fb6d39d6d3ba03704f3fc
Daniyal Ibrayev <dan.lalka228@gmail.com>
2024-11-13 14:55:00 +0500

    finished script 6

---
+++ classwork_4/student/script 6.js
@@ -0,0 +1,27 @@
+let dayNumber = 8;
+
+switch (dayNumber) {
+    case 1:
+        console.log('Понедельник');
+        break;
+    case 2:
+        console.log('Вторник');
+        break;
+    case 3:
+        console.log('Среда');
+        break;
+    case 4:
+        console.log('Четверг');
+        break;
+    case 5:
+        console.log('Пятница');
+        break;
+    case 6:
+        console.log('Суббота');
+        break;
+    case 7:
+        console.log('Воскресенье');
+        break;
+    default:
+        console.log('Неверный номер дня')
+}
